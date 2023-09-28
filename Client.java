b7202b92c6289c872e86111458a0cbece9999819
Fogolar1 <enzkf@outlook.com>
2023-09-28 10:52:01 -0300

    envia index.html através de socket

--- Client.java
+++ Client.java
@@ -1,6 +1,4 @@
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.net.Socket;
 import java.util.Scanner;
 
@@ -11,15 +9,26 @@ public class Client {
     public static void start(){
         try(Socket socket = new Socket("127.0.0.1", 3030)){
             DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
-            DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
-            while (true){
-                String messagem = scanner.nextLine();
-                dataOutputStream.writeUTF(messagem);
-                if(messagem.equals("exit")){
-                    System.out.println("Closing connection");
-                    break;
+            int fileNameLength = dataInputStream.readInt();
+            if(fileNameLength > 0){
+                byte[] fileNameBuffer = new byte[fileNameLength];
+                dataInputStream.readFully(fileNameBuffer, 0, fileNameBuffer.length);
+                String fileName = new String(fileNameBuffer);
+                System.out.println("File name: " + fileName);
+
+                int fileContentLength = dataInputStream.readInt();
+                if(fileContentLength > 0){
+                    byte[] fileContentBytes = new byte[fileContentLength];
+                    dataInputStream.readFully(fileContentBytes, 0, fileContentLength);
+
+                    File download = new File("C:\\Users\\ENZO.KF\\" + fileName);
+                    System.out.println(download.getAbsolutePath());
+                    FileOutputStream fileOutputStream = new FileOutputStream(download);
+                    fileOutputStream.write(fileContentBytes);
+                    fileOutputStream.close();
                 }
             }
+
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
--- Server.java
+++ Server.java
@@ -1,6 +1,7 @@
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.file.Files;
 
 public class Server {
 
@@ -9,20 +10,41 @@ public class Server {
     public void start(){
         try (ServerSocket serverSocket = new ServerSocket(port)){
             Socket client = serverSocket.accept();
-            DataInputStream dataInputStream = new DataInputStream(client.getInputStream());
+            File file = createHtmlFile(client.getRemoteSocketAddress().toString());
+            FileInputStream fileInputStream = new FileInputStream(file);
             DataOutputStream dataOutputStream = new DataOutputStream(client.getOutputStream());
 
-            while(true){
-                String message = dataInputStream.readUTF();
-                System.out.println("Message received: " + message);
-                if(message.equals("exit")){
-                    System.out.println("Closing connection");
-                    break;
-                }
-            }
-            client.close();
+            String fileName = file.getName();
+            byte[] bufferName = fileName.getBytes();
+            byte[] bufferContent = new byte[(int) file.length()];
+            fileInputStream.read(bufferContent);
+
+            dataOutputStream.writeInt(bufferName.length);
+            dataOutputStream.write(bufferName);
+
+            dataOutputStream.writeInt(bufferContent.length);
+            dataOutputStream.write(bufferContent);
         } catch (IOException e) {
+            e.printStackTrace();
             throw new RuntimeException(e);
         }
+    }
+
+    public File createHtmlFile(String ip) throws IOException {
+        File file = new File("C:\\Users\\ENZO.KF\\IdeaProjects\\httpServer\\template.html");
+        String content  = new String(Files.readAllBytes(file.toPath()));
+        String html = content
+                .replace("{{ip}}", ip)
+                .replace("{{porta}}", "Teste");
+        File htmlFile = new File("C:\\Users\\ENZO.KF\\IdeaProjects\\httpServer\\index.html");
+        BufferedWriter writer = new BufferedWriter(new FileWriter(htmlFile));
+        writer.write(html);
+        writer.close();
+
+        return htmlFile;
+    }
+
+    public void redirectPort(Socket client){
+
     }
 }
